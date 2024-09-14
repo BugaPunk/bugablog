4854484169068a60bae379e7df34f1a3dfcbcb4e
BugaPunks <punksbuga@outlook.es>
2024-09-13 21:26:36 -0400

    ProyectoIniciado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import UnoCSS from 'unocss/astro';
+
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site:'http://localhost:4321/',
+  trailingSlash: 'ignore',
+  integrations: [sitemap(), UnoCSS({ injectReset: true }), tailwind()],
+  vite: {
+    optimizeDeps: {
+      exclude: ['@resvg/resvg-js'],
+    },
+  },
+});
